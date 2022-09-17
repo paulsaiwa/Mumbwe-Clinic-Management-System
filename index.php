c1aa800f76ed2070b3a3b52a06165531b9329208
Geoffrey Maziya <gemaziya@EA.HOLCIM.NET>
2022-09-17 23:21:07 +0200

    updated

--- index.php
+++ index.php
@@ -3,27 +3,27 @@
 <title>Home</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="Assets/css/moodal.css">
+<link rel="stylesheet" href="Assets/css/style.css">
 <body>
 
-		<div class="w3-container w3-cyan">
-		 	<div class="w3-container w3-red">
-		 		<img src="Assets/img/mw_logo.png" width="100" height="100" class="w3-round-xxlarge w3-left"> 
-		 		 <img src="Assets/img/logo.png" width="100" height="100" class="w3-round-xxlarge w3-right"> 
-		 		
-		 		
-		 	</div> 
-		  <h1 class="">MUMBWE CLINIC MANAGEMENT SYSTEM</h1>
+		<div class="w3-container w3-cyan" id="header">
+		 	
+		 		<img src="Assets/img/mw_logo.png" width="100" height="100" class="w3-round-xxlarge w3-display-topleft"> 
+		 		<h1 class="w3-center">MUMBWE CLINIC MANAGEMENT SYSTEM</h1>
+		 		 <img src="Assets/img/logo.png" width="100" height="100" class="w3-round-xxlarge w3-display-topright"> 
+		 			 		
+		 			  
 		</div>
 
 		
-		<div class="w3-container">
+		<div class="w3-container w3-center" id="login">
 		  <p>
-		  	<form class="w3-container">
+		  	<form class="w3-container ">
 		  		<label>First Name</label>
-				<input class="w3-input" type="text" name="username" >
+				<input class="w3-input w3-center" type="text" name="username" id="inp" >
 				<label>Last Name</label>
-				<input class="w3-input" type="text" name="password">
-				<input class="w3-btn" type="reset" name="reset" value="Cancel">
+				<input class="w3-input" type="text" name="password" id="inp">
+				<input class="w3-btn " type="reset" name="reset" value="Cancel">
 				<input class="w3-btn" type="submit" name="login" value="Login">
 
 				
