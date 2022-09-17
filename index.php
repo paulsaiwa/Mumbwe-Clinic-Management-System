2ab0e94ab9dd24bc8b4c3327562a68c7d1042129
Geoffrey Maziya <gemaziya@EA.HOLCIM.NET>
2022-09-17 22:46:24 +0200

    files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<title>Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Assets/css/moodal.css">
+<body>
+
+		<div class="w3-container w3-cyan">
+		 	<div class="w3-container w3-red">
+		 		<img src="Assets/img/mw_logo.png" width="100" height="100" class="w3-round-xxlarge w3-left"> 
+		 		 <img src="Assets/img/logo.png" width="100" height="100" class="w3-round-xxlarge w3-right"> 
+		 		
+		 		
+		 	</div> 
+		  <h1 class="">MUMBWE CLINIC MANAGEMENT SYSTEM</h1>
+		</div>
+
+		
+		<div class="w3-container">
+		  <p>
+		  	<form class="w3-container">
+		  		<label>First Name</label>
+				<input class="w3-input" type="text" name="username" >
+				<label>Last Name</label>
+				<input class="w3-input" type="text" name="password">
+				<input class="w3-btn" type="reset" name="reset" value="Cancel">
+				<input class="w3-btn" type="submit" name="login" value="Login">
+
+				
+
+		</form>
+		  </p>
+		</div>
+
+		<div class="w3-container w3-cyan">
+		  <h5>Footer</h5>
+		</div>
+
+</body>
